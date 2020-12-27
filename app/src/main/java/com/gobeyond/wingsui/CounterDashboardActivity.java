864d39c0822de4ed9e9dcb59441a35572614291d
RakinShafqatChowdhury <rakinshafqat@gmail.com>
2020-12-27 11:14:58 +0600

    sla missed orders itemview layout added.

--- app/src/main/java/com/gobeyond/wingsui/CounterDashboardActivity.java
+++ app/src/main/java/com/gobeyond/wingsui/CounterDashboardActivity.java
@@ -9,6 +9,6 @@ public class CounterDashboardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_counter_dashboard);
+        setContentView(R.layout.itemview_sla_missed_orders);
     }
 }
